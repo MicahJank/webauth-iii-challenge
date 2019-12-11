7cf1a65335b8ee68af95814091807c9835c123a3
MicahJank <MicahJank@users.noreply.github.com>
2019-12-11 09:54:54 -0600

    added the users-router

--- users/users-router.js
+++ users/users-router.js
@@ -2,12 +2,23 @@ const router = require('express').Router();
 
 const Users = require('./users-model.js');
 const restricted = require('../auth/restricted-middleware.js');
-const checkRole = require('../auth/check-role-middleware.js');
+const checkDepartment = require('../auth/check-role-middleware.js');
 
-router.get('/', restricted, checkRole('Admin'), (req, res) => {
+router.get('/departmentA', restricted, checkDepartment('Department A'), (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+
+router.get('/departmentB', restricted, checkDepartment('Department B'), (req, res) => {
+  // Users.find()
+  //   .then(users => {
+  //     res.json(users);
+  //   })
+  //   .catch(err => res.send(err));
+});
+
+module.exports = router;
