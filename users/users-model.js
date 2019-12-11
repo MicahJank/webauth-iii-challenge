5732f58db033e45b8d762dfdbd7c7be36c4e3fdb
MicahJank <MicahJank@users.noreply.github.com>
2019-12-11 09:50:45 -0600

    created the users model helper functions file

---
+++ users/users-model.js
@@ -0,0 +1,32 @@
+const db = require('../config/dbConfig.js');
+
+module.exports = {
+    add,
+    find,
+    findBy,
+    findById
+};
+
+
+function find() {
+    return db('users').select('id', 'username', 'password', 'departments');
+  }
+  
+  function findBy(filter) {
+    return db('users').where(filter);
+  }
+  
+function add(user) {
+    // const [id] = await db('users').insert(user);
+    return db('users').insert(user)
+                .then(ids => {
+                    const [id] = ids;
+                    return findById(id);  
+                });
+  }
+  
+  function findById(id) {
+    return db('users')
+      .where({ id })
+      .first();
+  }
